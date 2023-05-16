44aa0d14a9ccb8139b18f4a0f857149d312b136c
user <user@BOOK-LCCUUB9T5H>
2023-05-16 09:51:46 +0900

    4

--- Jdbc/src/xyz/itwill/jdbc/PreparedStatementApp중요.java
+++ Jdbc/src/xyz/itwill/jdbc/PreparedStatementApp중요.java
@@ -11,7 +11,7 @@ import java.sql.ResultSet;
 // => InParameter를 사용하여 가독성이 향상되고 유지보수의 효율성 증가
 // => InSQL 해킹 기술을 무효화 처리 - InParameter로 전달받은 사용자 입력값은 SQL 명령에서 무조건 문자값으로 처리
 //단점 : 하나의 PreparedStatement는 저장된 하나의 SQL 명령만 전달하여 실행 가능
-public class PreparedStatementApp {
+public class PreparedStatementApp중요 {
 	public static void main(String[] args) throws Exception {
 		/*
 		//키보드로 학생정보를 입력받아 STUDENT 테이블에 삽입하고 STUDENT 테이블에 저장된
@@ -21,47 +21,47 @@ public class PreparedStatementApp {
 		BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
 		
 		//키보드로 학생정보를 입력받아 저장
-		System.out.println("<<학생정보 입력>>");
-		System.out.print("학번 입력 >> ");
-		int no=Integer.parseInt(in.readLine());
-		System.out.print("이름 입력 >> ");
-		String name=in.readLine();
-		System.out.print("전화번호 입력 >> ");
-		String phone=in.readLine();
-		System.out.print("주소 입력 >> ");
-		String address=in.readLine();
-		System.out.print("생년월일 입력 >> ");
-		String birthday=in.readLine();
-		System.out.println("==============================================================");
+			System.out.println("<<학생정보 입력>>");
+			System.out.print("학번 입력 >> ");
+			int no=Integer.parseInt(in.readLine());
+			System.out.print("이름 입력 >> ");
+			String name=in.readLine();
+			System.out.print("전화번호 입력 >> ");
+			String phone=in.readLine();
+			System.out.print("주소 입력 >> ");
+			String address=in.readLine();
+			System.out.print("생년월일 입력 >> ");
+			String birthday=in.readLine();
+			System.out.println("==============================================================");
 		//STUDENT 테이블에 키보드로 입력받은 학생정보를 삽입 처리
-		Connection con=ConnectionFactory.getConnection();
-		
+			Connection con=ConnectionFactory.getConnection();
+			
 		//Connection.prepareStatement(String sql) : Connection 객체로부터 SQL 명령이 저장된
 		//PreparedStatement 객체를 반환하는 메소드
 		// => PreparedStatement 객체에 저장되는 SQL 명령에는 ?(InParameter) 기호 사용
 		//InParameter : Java 변수값을 제공받아 SQL 명령의 문자값으로 표현하기 위한 기호
-		String sql1="insert into student values(?,?,?,?,?)";//불완전한 SQL 명령
-		PreparedStatement pstmt=con.prepareStatement(sql1);
+			String sql1="insert into student values(?,?,?,?,?)";//불완전한 SQL 명령
+			PreparedStatement pstmt=con.prepareStatement(sql1);
 		//PreparedStatement.setXXX(int parameterIndex, XXX value) 
 		// => PreparedStatement 객체에 저장된 SQL 명령의 InParameter에 Java 변수값을 전달하는 메소드
 		// => XXX : InParameter에 전달하기 위한 값에 대한 Java 자료형
 		// => parameterIndex : InParameter의 위치값(첨자) - 1부터 1씩 증가되는 정수값
 		// => 반드시 모든 InParameter에 Java 변수값을 전달받아 완전한 SQL 명령 완성
-		pstmt.setInt(1, no);
-		pstmt.setString(2, name);
-		pstmt.setString(3, phone);
-		pstmt.setString(4, address);
-		pstmt.setString(5, birthday);
-		
+			pstmt.setInt(1, no);
+			pstmt.setString(2, name);
+			pstmt.setString(3, phone);
+			pstmt.setString(4, address);
+			pstmt.setString(5, birthday);
+			
 		//PreparedStatement.executeUpdate() : PreparedStatement 객체에 저장된 DML 명령을
 		//전달하여 실행하고 조작행을 갯수를 정수값(int)로 반환하는 메소드
-		int rows=pstmt.executeUpdate();
+			int rows=pstmt.executeUpdate();
 		
 		System.out.println("[결과]"+rows+"명의 학생정보를 삽입 하였습니다.");
 		System.out.println("==============================================================");
 		//STUDENT 테이블에 저장된 모든 학생정보를 검색하여 출력 처리
-		String sql2="select * from student order by no";
-		pstmt=con.prepareStatement(sql2);
+			String sql2="select * from student order by no";
+			pstmt=con.prepareStatement(sql2);
 		
 		//PreparedStatement.executeUpdate() : PreparedStatement 객체에 저장된 SELECT 명령을
 		//전달하여 실행하고 모든 검색행이 저장된 ResultSet 객체를 반환하는 메소드
