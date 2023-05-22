85ad4584198cf7f623323149aca6e5912507f03f
user <user@BOOK-LCCUUB9T5H>
2023-05-22 13:18:18 +0900

    4444

--- Jdbc/src/xyx/itwill/teamproject/JdbcDAO.java
+++
@@ -1,4 +0,0 @@
-package xyx.itwill.teamproject;
-
-public class JdbcDAO {
-}
--- Jdbc/src/xyx/itwill/teamproject/OrderprogramDAO.java
+++
@@ -1,8 +0,0 @@
-package xyx.itwill.teamproject;
-
-public class OrderprogramDAO {
-	public static void main(String[] args) {
-		
-		
-	}
-}
