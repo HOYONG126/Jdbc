2562688366117b6bafd54d4236957e5636dadebd
user <user@BOOK-LCCUUB9T5H>
2023-05-22 13:18:39 +0900

    4

--- Jdbc/src/xyz/itwill/student/StudentCUIApp.java
+++ Jdbc/src/xyz/itwill/student/StudentCUIApp.java
@@ -8,7 +8,7 @@ import java.util.regex.Pattern;
 //학생정보를 관리하는 프로그램 작성
 // => 메뉴 선택에 따른 학생정보 삽입,변경,삭제,검색 기능 제공
 // => 입력과 출력은 프로그램에서 구현하고 데이타 처리는 DAO 클래스의 메소드를 호출하여 처리
-public class StudentCUIApp {
+public class StudentCUIApp { 
 	//키보드 입력스트림을 저장하기 위한 필드
 	private BufferedReader in;
 	
